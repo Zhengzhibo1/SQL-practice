07618dc7cab2db9e1db4e40b064d0175c36cf114
Zhengzhibo1 <60970579+Zhengzhibo1@users.noreply.github.com>
2020-07-15 20:12:19 +0800

    Create solution.sql

---
+++ 02_查找入职员工时间排名倒数第三的员工所有信息/solution.sql
@@ -0,0 +1,11 @@
+/*
+查找入职员工时间排名倒数第三的员工所有信息，为了减轻入门难度，目前所有的数据里员工入职的日期都不是同一天
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+*/
