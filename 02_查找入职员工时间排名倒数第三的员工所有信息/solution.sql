b261a0659efa89933a5424c2b7af509f6083fee6
Zhengzhibo1 <60970579+Zhengzhibo1@users.noreply.github.com>
2020-07-15 20:21:31 +0800

    Update solution.sql

--- 02_查找入职员工时间排名倒数第三的员工所有信息/solution.sql
+++ 02_查找入职员工时间排名倒数第三的员工所有信息/solution.sql
@@ -9,3 +9,12 @@ CREATE TABLE `employees` (
 `hire_date` date NOT NULL,
 PRIMARY KEY (`emp_no`));
 */
+
+/*方法1：考虑重复情况，利用distinct关键字去重*/
+SELECT * FROM employees WHERE hire_date = (SELECT DISTINCT hire_date FROM employees ORDER BY hire_date DESC LIMIT 2,1);
+/*方法2：不考虑重复情况
+其中LIMIT 带1个参数表示，从头开始取几条记录
+LIMIT 带2个参数表示，从第一个参数的位置起，往后取第二参数条记录
+LIMIT 与 OFFSET 一起用，表示从OFFSET的位置开始取第一个参数条记录*/
+SELECT * FROM employees ORDER BY hire_date DESC LIMIT 2,1;
+SELECT * FROM employees ORDER BY hire_date DESC LIMIT 1 OFFSET 2;
