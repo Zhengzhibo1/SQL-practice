1cb57004b07a9e904108586aa945f50a9cce2dff
Zhengzhibo1 <60970579+Zhengzhibo1@users.noreply.github.com>
2020-07-16 10:15:07 +0800

    Create solution.sql

---
+++ 07_查找薪水变动超过15次的员工号emp_no以及其对应的变动次/solution.sql
@@ -0,0 +1,14 @@
+/*
+查找薪水变动超过15次的员工号emp_no以及其对应的变动次数t
+CREATE TABLE `salaries` (
+`emp_no` int(11) NOT NULL,
+`salary` int(11) NOT NULL,
+`from_date` date NOT NULL,
+`to_date` date NOT NULL,
+PRIMARY KEY (`emp_no`,`from_date`));
+*/
+
+/*方法1 分组，然后统计次数*/
+SELECT emp_no, Count(*) AS t 
+FROM salaries
+GROUP BY emp_no HAVING t > 15;
