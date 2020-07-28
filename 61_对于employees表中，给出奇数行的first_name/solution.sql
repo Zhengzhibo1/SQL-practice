0667cf314908d0437b67f62404aa3b297d49b9b1
Zhengzhibo1 <60970579+Zhengzhibo1@users.noreply.github.com>
2020-07-28 16:36:00 +0800

    Create solution.sql

---
+++ 61_对于employees表中，给出奇数行的first_name/solution.sql
@@ -0,0 +1,24 @@
+/*
+对于employees表中，输出first_name排名(按first_name升序排序)为奇数的first_name
+CREATE TABLE `employees` (
+`emp_no` int(11) NOT NULL,
+`birth_date` date NOT NULL,
+`first_name` varchar(14) NOT NULL,
+`last_name` varchar(16) NOT NULL,
+`gender` char(1) NOT NULL,
+`hire_date` date NOT NULL,
+PRIMARY KEY (`emp_no`));
+如，输入为：
+INSERT INTO employees VALUES(10001,'1953-09-02','Georgi','Facello','M','1986-06-26');
+INSERT INTO employees VALUES(10002,'1964-06-02','Bezalel','Simmel','F','1985-11-21');
+INSERT INTO employees VALUES(10005,'1955-01-21','Kyoichi','Maliniak','M','1989-09-12');
+INSERT INTO employees VALUES(10006,'1953-04-20','Anneke','Preusig','F','1989-06-02');
+*/
+
+/*方法1 嵌套子查询，对于每一个first_name，查询下排名是否为奇数*/
+SELECT e1.first_name
+FROM employees AS e1
+WHERE (SELECT COUNT(*)
+       FROM employees AS e2
+       WHERE e1.first_name >= e2.first_name
+      ) & 1 = 1;
